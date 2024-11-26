f5a2b23885340e21f95bb7a597d437b785012100
Philippe Lebel <phil.dlebel.eco@gmail.com>
2024-11-26 13:16:29 -0500

    servo rewrite

--- code_robot/code_robot.ino
+++ code_robot/code_robot.ino
@@ -1,10 +1,11 @@
 #include <PS4Controller.h>
 #include <ESP32Servo.h>
+#include <cmath>
 
 // Pin Definitions
 #define CONTINUOUS_SERVO_PIN 33
 #define SERVO_180_PIN_1 25
-#define SERVO_180_PIN_2 14
+#define SERVO_180_PIN_2 12
 
 #define PWM_RIGHT_PIN 4
 #define RIGHT_OUTPUT_4_PIN 16
@@ -18,21 +19,20 @@ Servo continuousServo;
 Servo servo180_1;
 Servo servo180_2;
 
-// Protocol State
-enum Protocol { SERVO, DC_MOTOR };
-Protocol currentProtocol = SERVO;
+
 
 // Joystick Dead Zone
 const int DEAD_ZONE = 20;
 
 // Servo Parameters
 int continuousServoPulseWidth = 1500; // Neutral pulse width
-int servo180_1PulseWidth = 1500; // Neutral position
-int servo180_2PulseWidth = 1500; // Neutral position
+int servo180_1PulseWidth = 90; // Neutral position
+int servo180_2PulseWidth = 90; // Neutral position
 
 // PWM Parameters
 int pwmRight = 0;
 int pwmLeft = 0;
+float leftAxisValuePrev = 2;
 
 // Keep-alive interval (in milliseconds)
 const unsigned long KEEP_ALIVE_INTERVAL = 5000;
@@ -43,11 +43,13 @@ unsigned long lastKeepAliveTime = 0;
 void setupServos();
 void setupDCMotors();
 void handleServoProtocol();
-void handleDCMotorProtocol();
 void switchProtocol();
+void rotateContinuousServo(Servo servoName, float stickValue);
+void rotate180Servo(Servo servoName, float stickValue);
+
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(57600);
   PS4.begin(); // Initialize PS4 controller
   setupServos();
   setupDCMotors();
@@ -56,14 +58,15 @@ void setup() {
 void loop() {
   if (PS4.isConnected()) {
     Serial.println("PS4 Controller is connected.");
-
     // Send a keep-alive signal every KEEP_ALIVE_INTERVAL milliseconds
     unsigned long currentTime = millis();
     if (currentTime - lastKeepAliveTime >= KEEP_ALIVE_INTERVAL) {
       PS4.setLed(0, 0, 255); // Example keep-alive command: set LED color
+      
       lastKeepAliveTime = currentTime;
     }
     handleServoProtocol();
+    delay(100);
   } else {
     Serial.println("Waiting for PS4 Controller...");
     delay(1000); // Delay to prevent spamming the serial monitor
@@ -85,27 +88,47 @@ void setupDCMotors() {
   pinMode(LEFT_OUTPUT_1_PIN, OUTPUT);
 }
 
+
+
+void rotateContinuousServo(Servo servoName, float stickValue){
+
+  if (abs(stickValue) > 0.13) {
+    continuousServoPulseWidth = 1500 + (500 * stickValue); // Adjust pulse width based on joystick
+  } else {
+    continuousServoPulseWidth = 1500; // Stop
+  }
+  servoName.writeMicroseconds(continuousServoPulseWidth);
+}
+
+void rotate180Servo(Servo servoName, float rotation){
+  int angle = servoName.read();
+  int continuousServoAngle = angle + rotation;
+  continuousServoAngle = constrain(continuousServoAngle, 0, 180);
+  servoName.write(continuousServoAngle);
+
+}
+
 void handleServoProtocol() {
   // Parameters for 180-degree servos
   const int pulseWidth180 = 2500; // Adjusted max pulse width
   const int pulseWidth0 = 500;   // Min pulse width
   const float joystickStep = 0.035; // Adjust step size for joystick
-  const float buttonStep = 0.05;
-  const float buttonStep1 = 20;   // Adjust step size for buttons
+  const float buttonStep = 2;
+  const float buttonStep1 = 2;   // Adjust step size for buttons
 
   const int pwm_min = 90;
+  int servo1Rotation = 0;
+  int servo2Rotation = 0;
 
   // Continuous Rotation Servo Control (Left Joystick)
   int rightJoystickY = PS4.RStickY();
   float leftAxisValue = rightJoystickY / 128.0; // Normalize joystick value to -1.0 to 1.0
 
-  if (abs(leftAxisValue) > 0.13) {
-    continuousServoPulseWidth = 1500 + (500 * leftAxisValue); // Adjust pulse width based on joystick
-  } else {
-    continuousServoPulseWidth = 1500; // Stop
-  }
-  continuousServo.writeMicroseconds(continuousServoPulseWidth);
 
+  if (fabs(leftAxisValue - leftAxisValuePrev) <= 0.03) {
+    rotateContinuousServo(continuousServo, leftAxisValue);
+  }
+  leftAxisValuePrev = leftAxisValue;
   Serial.print("Left Joystick Y: ");
   Serial.print(rightJoystickY);
   Serial.print(", Continuous Servo Pulse Width: ");
@@ -114,8 +137,8 @@ void handleServoProtocol() {
 
 
   // 180-Degree Servo Control (L2 and R2 Buttons)
-  float l2Value = PS4.L2(); // Read L2 value as a float between 0 and 128
-  float r2Value = PS4.R2(); // Read R2 value as a float between 0 and 128
+  float l2Value = PS4.L2(); // Read L2 value as a float between 0 and 1
+  float r2Value = PS4.R2(); // Read R2 value as a float between 0 and 1
 
   Serial.print("L2 Value: ");
   Serial.print(l2Value);
@@ -124,52 +147,47 @@ void handleServoProtocol() {
 
  
 
+
   if (l2Value > 0.1 && r2Value > 0.1) {
     // Both buttons pressed, do not rotate
     // Hold position
   } else if (l2Value > 0.1) {
     // Move toward 0 degrees
-    servo180_1PulseWidth += -servo180_1PulseWidth * l2Value * buttonStep;
+    servo1Rotation =  -l2Value * buttonStep;
   } else if (r2Value > 0.1) {
     // Move toward 180 degrees
-    servo180_1PulseWidth += servo180_1PulseWidth * r2Value * buttonStep;
+    servo1Rotation = r2Value * buttonStep;
   }
 
-  servo180_1PulseWidth = constrain(servo180_1PulseWidth, pulseWidth0, pulseWidth180);
+  if (servo1Rotation != 0){
+    rotate180Servo(servo180_1, servo1Rotation);
+  }
 
-  servo180_1.writeMicroseconds(servo180_1PulseWidth);
-
-  Serial.print("Servo 180_1 Pulse Width: ");
-  Serial.println(servo180_1PulseWidth);
 
   // 180-Degree Servo Control (L2 and R2 Buttons)
-  float l1Value = PS4.L1(); // Read L2 value as a float between 0 and 1
-  float r1Value = PS4.R1(); // Read R2 value as a float between 0 and 1
+  int l1Value = PS4.L1(); // Read L2 value as a float between 0 and 1
+  int r1Value = PS4.R1(); // Read R2 value as a float between 0 and 1
 
   Serial.print("L1 Value: ");
   Serial.print(l1Value);
   Serial.print(", R1 Value: ");
   Serial.println(r1Value);
 
-  if (l1Value > 0.1 && r1Value > 0.1) {
+  if (l1Value == 1 && r1Value == 1) {
     // Both buttons pressed, do not rotate
     // Hold position
 
-  } else if (l1Value > 0.1) {
+  } else if (l1Value == 1) {
     // Move toward 0 degrees
-    servo180_2PulseWidth += -servo180_2PulseWidth * l1Value * buttonStep1;
-
-  } else if (r1Value > 0.1) {
+    servo2Rotation =  -l1Value * buttonStep1;
+  } else if (r1Value == 1) {
     // Move toward 180 degrees
-    servo180_2PulseWidth += servo180_2PulseWidth * r1Value * buttonStep1;
-   
+    servo2Rotation =  r1Value * buttonStep1;
   }
-  servo180_2PulseWidth = constrain(servo180_2PulseWidth, pulseWidth0, pulseWidth180);
 
-  servo180_2.writeMicroseconds(servo180_2PulseWidth);
-
-  Serial.print("Servo 180_2 Pulse Width: ");
-  Serial.println(servo180_2PulseWidth);
+  if (servo2Rotation != 0){
+    rotate180Servo(servo180_2, servo2Rotation);
+  }
 
     // Read Joystick Inputs
   int forwardBackward = PS4.LStickY();
